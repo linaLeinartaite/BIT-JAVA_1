bc3ee98f5c4d1b5b39d8019946842177c0a51f15
linaLeinartaite <lleinartaite@yahoo.com>
2020-01-31 10:47:14 +0200

    hello world added

---
+++ 20200128_HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Lina
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        //sout + tab duoda  >> System.out.println("");
+
+        int a;  // int yra sveiki skaiciai;
+        a = 3;    // = yra priskirimo operatorius;
+
+        int a1 = 5;
+        a = a + 1;
+        int b = a + a1;
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println(b);
+        System.out.println(a*b);
+
+        //alt+shift+F graziai suformatuoja texta
+    }
+
+}
